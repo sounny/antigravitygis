b3f31f784e0122208fd11f9d20a6cfec69b6d82d
Sounny <sounny@sounny.com>
2026-08-14 00:45:03 +0200

    Add update.ps1 alias

---
+++ update.ps1
@@ -0,0 +1,8 @@
+<#
+.SYNOPSIS
+1-Line Updater for AntigravityGIS
+Usage in PowerShell:
+irm https://raw.githubusercontent.com/sounny/antigravitygis/main/update.ps1 | iex
+#>
+
+irm https://raw.githubusercontent.com/sounny/antigravitygis/main/install.ps1 | iex
